9692400ca513783f21bf2428b8ccb1a68bf55a4f
govind6498 <govindsingh6498@gmail.com>
2021-11-19 19:54:43 +0530

    Added:To initialize project structure

---
+++ src/main/java/com/bridgelabz/springproject/demo/SpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.springproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Spring Concept Demo");
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
